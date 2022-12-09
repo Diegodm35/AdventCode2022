6034094ed6b1656fea9257c3d9f75858637c4d0c
Diegodm35 <alu0101337975@ull.edu.es>
2022-12-09 18:08:42 +0000

    Refactor

--- Day01/Day01/Program.cs
+++ Day01/Day01/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,54 +13,21 @@ namespace Day01
     {
         static void Main(string[] args)
         {
-            problema1();
-            problema2();
+            string[] file = System.IO.File.ReadAllLines("C:\\Users\\Diegodm\\Projects\\AdventCode2022\\Day01\\Day01\\input.txt");
+            problema1(file);
+            problema2(file);
         }
 
-        private static void problema2()
+        
+        static void problema1(string[] file)
         {
-            // Guardar en un array cada valor
-            string[] filePath = System.IO.File.ReadAllLines("C:\\Users\\Diegodm\\Projects\\AdventCode2022\\Day01\\Day01\\input.txt");
-            // Se convertira cada valor a int y se sumara a un acumulador
-            // Si el valor no se puede parsear
-            // guardar el valor en un array
-            // reiniciar el acumulador
-            // Ordenar
-            int sum = 0;
-            var sumList = new ArrayList();
-            int index = 0;
-            foreach (var item in filePath)
-            {
-                int number;
-                if (int.TryParse(item, out number))
-                {
-                    sum += number;
-                }
-                else
-                {
-                    sumList.Add(sum);
-                    sum = 0;
-                    index++;
-                }
-            }
-            sumList.Sort();
-            // sum the 3 last elements of an arraylist 
-            int sumMax = (int)sumList[--index] + (int)sumList[--index] + (int)sumList[--index];
-            System.Console.WriteLine("Suma: " + sumMax);
-            System.Console.ReadKey();
-        }
-
-        static void problema1()
-        {
-            // Guardar en un array cada valor
-            string[] filePath = System.IO.File.ReadAllLines("C:\\Users\\Diegodm\\Projects\\AdventCode2022\\Day01\\Day01\\input.txt");
             // Se convertira cada valor a int y se sumara a un acumulador
             // Si el valor no se puede parsear
             // comparar con este valor con el valor maximo y guardar el mayor
             // reiniciar el acumulador
             int sum = 0;
             int maxSum = 0;
-            foreach (var item in filePath)
+            foreach (var item in file)
             {
                 int number;
                 if (int.TryParse(item, out number))
@@ -76,6 +44,37 @@ namespace Day01
                 }
             }
             System.Console.WriteLine("Suma: " + maxSum);
+            System.Console.ReadKey();
+        }
+        private static void problema2(string[] file)
+        {
+            // Se convertira cada valor a int y se sumara a un acumulador
+            // Si el valor no se puede parsear
+            // guardar el valor en un array
+            // reiniciar el acumulador
+            // Ordenar
+            int sum = 0;
+            var sumList = new List<int>();
+            int index = 0;
+            foreach (var item in file)
+            {
+                int number;
+                if (int.TryParse(item, out number))
+                {
+                    sum += number;
+                }
+                else
+                {
+                    sumList.Add(sum);
+                    sum = 0;
+                    index++;
+                }
+            }
+            sumList.Sort();
+            // sum the 3 last elements of an arraylist 
+            // 
+            int sumMax = sumList.Skip(Math.Max(0, sumList.Count() - 3)).Take(3).Sum();
+            System.Console.WriteLine("Suma: " + sumMax);
             System.Console.ReadKey();
         }
     }
