00f792a43582364e67ef96c774b5565b28628d8a
Diegodm35 <alu0101337975@ull.edu.es>
2022-12-10 20:22:17 +0000

    Refactor

--- Day01/Day01/Program.cs
+++ Day01/Day01/Program.cs
@@ -16,15 +16,12 @@ namespace Day01
             string[] file = System.IO.File.ReadAllLines("C:\\Users\\Diegodm\\Projects\\AdventCode2022\\Day01\\Day01\\input.txt");
             problema1(file);
             problema2(file);
+            Console.ReadKey();
         }
 
         
         static void problema1(string[] file)
         {
-            // Se convertira cada valor a int y se sumara a un acumulador
-            // Si el valor no se puede parsear
-            // comparar con este valor con el valor maximo y guardar el mayor
-            // reiniciar el acumulador
             int sum = 0;
             int maxSum = 0;
             foreach (var item in file)
@@ -43,19 +40,12 @@ namespace Day01
                     sum = 0;
                 }
             }
-            System.Console.WriteLine("Suma: " + maxSum);
-            System.Console.ReadKey();
+            Console.WriteLine("Suma: " + maxSum);
         }
         private static void problema2(string[] file)
         {
-            // Se convertira cada valor a int y se sumara a un acumulador
-            // Si el valor no se puede parsear
-            // guardar el valor en un array
-            // reiniciar el acumulador
-            // Ordenar
             int sum = 0;
             var sumList = new List<int>();
-            int index = 0;
             foreach (var item in file)
             {
                 int number;
@@ -67,15 +57,11 @@ namespace Day01
                 {
                     sumList.Add(sum);
                     sum = 0;
-                    index++;
                 }
             }
             sumList.Sort();
-            // sum the 3 last elements of an arraylist 
-            // 
-            int sumMax = sumList.Skip(Math.Max(0, sumList.Count() - 3)).Take(3).Sum();
-            System.Console.WriteLine("Suma: " + sumMax);
-            System.Console.ReadKey();
+            int sumMax = sumList.Skip(Math.Max(0, sumList.Count() - 3)).Sum();
+            Console.WriteLine("Suma: " + sumMax);
         }
     }
 }
